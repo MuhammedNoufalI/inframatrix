7d9d4fe4ba099b000b25b20773f54079586f4ead
Muhammed Noufal <muhammednoufal7@gmail.com>
2026-02-22 13:11:11 +0400

    fix: aggressively force root URL and scheme for proxy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
+            if (config('app.url')) {
+                \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+            }
         }
 
         \Illuminate\Support\Facades\Gate::before(function ($user, $ability) {
