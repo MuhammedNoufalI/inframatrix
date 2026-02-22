9dc00e31fb0e3aec796612dc556132fc26e8da72
Muhammed Noufal <muhammednoufal7@gmail.com>
2026-02-22 18:53:12 +0400

    fix: remove explicit headers in trustProxies to avoid undefined constant error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(
-            at: env('TRUSTED_PROXIES', '*'),
-            headers: \Symfony\Component\HttpFoundation\Request::HEADER_X_FORWARDED_ALL,
-        );
+        $middleware->trustProxies(at: env('TRUSTED_PROXIES', '*'));
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
